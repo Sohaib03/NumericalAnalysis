d68fce6dbb487113a9e3e28ee197c22b5f42a7fd
Sohaib03 <sohaibbinmusa@gmail.com>
2021-03-16 11:54:44 +0600

    Offline update

--- Offline1/bisectionMethod.py
+++ Offline1/bisectionMethod.py
@@ -1,5 +1,7 @@
 from math import sqrt
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 k = 0.5
 pt = 3
@@ -14,9 +16,39 @@ table = {
 }
 
 def f(x):
+    if (x == 0):
+        return 0
+    if (x == 1):
+        return np.NaN
     return (x / (1 - x))  * sqrt((2 * pt) / (2 + x)) - k
 
+def clearTable():
+    for key in table.keys():
+        table[key] = []
 
+def plotFunction():
+    x_cord = np.linspace(-1.9999,1, 100)
+    y = np.array([f(i) for i in x_cord] )
+    fig = plt.figure()
+    ax = plt.gca() 
+    ax.spines['top'].set_color('none')  
+    ax.spines['left'].set_position('zero') 
+    ax.spines['right'].set_color('none') 
+    ax.spines['bottom'].set_position('zero') 
+    
+    plt.xlim(-2, 2) 
+    plt.ylim(-2, 2)
+    plt.plot(x_cord, y) 
+    plt.grid(True) 
+    plt.show() 
+    
+def plotError():
+    err = table["Error"]
+    x_cord = list(range(1, len(err) + 1))
+    plt.plot(x_cord, err)
+    plt.grid(True)
+    plt.show()
+    
 def bisection(low, high, error, max_iteration = 20):
     mid = (low + high) / 2
     
@@ -38,8 +70,17 @@ def bisection(low, high, error, max_iteration = 20):
         return bisection(mid, high, error, max_iteration - 1)
     
 
-print(bisection(0, 1, 0.005))
-#print(table)
+def bisectionWOError(low, high, max_iteration):
+    return bisection(low, high, 0, max_iteration)
+    
+    
+plotFunction()
 
+# From the plot we observe the root lies in between 0 and 1
+clearTable()
+print(bisection(0, 1, 0))
+clearTable()
+print(bisectionWOError(0, 1, 30))
+plotError()
 df = pd.DataFrame(table)
 print(df)
